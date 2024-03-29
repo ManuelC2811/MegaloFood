21ade391bdcb11acbbbf63131338af4d3b142745
gabok2003 <gabriel.fonseca.guerrero@gmail.com>
2024-03-29 17:42:50 -0500

    primera parte de la autenticación del usuario y creación de rutas

--- megalo-food/frontend/src/AppRoutes.tsx
+++ megalo-food/frontend/src/AppRoutes.tsx
@@ -1,12 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom"
 import Layout from "./layouts/layout";
+import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
 
 const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout>HomePage</Layout>} />
-      <Route path="/UserProfile" element={<span>Pagina de usuario</span>} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/"  element={<Layout showHero><HomePage /></Layout>} />
+            <Route path="auth-callback" element={<AuthCallbackPage/>} />
+            <Route path="/user-profile"  element={<Layout><UserProfilePage /></Layout>} />
+            <Route path="*"  element={<Navigate to="/" />} />
+        </Routes>
+    )
 };
+
 export default AppRoutes;
---
+++ megalo-food/frontend/src/components/Footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+
+    return(
+        <div className="bg-blue-500 py-10">
+            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+                <span className="text-3xl text-white font-bold tracking-tight">
+                    MegaloFood
+                </span>
+                <span className="text-white font-bold tracking-tight flex gap-4">
+                    <span>Politica de Privacidad</span>
+                    <span>Terminos de nuestro Servicio</span>
+                </span>
+            </div>
+        </div>
+    )
+};
+
+export default Footer;

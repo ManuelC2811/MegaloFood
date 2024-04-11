ce3056fcbdab8b0b9a58ed34fd45d5e0027324ad
AlvaradoJose <estebanalvarado01@gmail.com>
2024-04-10 21:14:37 -0500

    Correcion en menu usuario y enrrutamiento de logout

--- megalo-food/frontend/src/components/Hero.tsx
+++ megalo-food/frontend/src/components/Hero.tsx
@@ -2,16 +2,9 @@
 import hero1 from "../assets/hero1.jpg";
 const Hero = () => {
   return (
-    <div className="relative w-full flex justify-center">
-      <img
-        src={hero1}
-        className="w-full object-cover max-h-[550px] sm:max-h-[300px] md:max-h-[400px] lg:max-h-[550px]"
-        alt=""
-      />
-      <div className="absolute inset-0 bg-black opacity-30"></div>
+    <div>
+      <img src={hero1} className="w-full max-h-[550px] object-cover" alt="" />
     </div>
   );
 };
 export default Hero;
-4;
-3;
--- megalo-food/frontend/src/components/MobileNavLinks.tsx
+++ megalo-food/frontend/src/components/MobileNavLinks.tsx
@@ -3,24 +3,23 @@ import { Button } from "./ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const MobileNavLinks = () => {
-  const { logout } = useAuth0();
+    const { logout } = useAuth0();
 
-  return (
-    <>
-      <Link
-        to="/user-profile"
-        className="flex bg-white items-center font-semibold text-black font-raleway hover:text-gray-500"
-      >
-        Perfil
-      </Link>
-      <Button
-        onClick={() => logout()}
-        className="flex items-center px-3 font-semibold font-raleway bg-red-500 opacity-90 hover:bg-white hover:text-red-500 border border-red-500"
-      >
-        Cerrar Sesión
-      </Button>
-    </>
-  );
-};
+    const handleLogout = async () => {
+        await logout();
+        window.location.assign(window.location.origin);
+    };
+
+    return(
+        <>
+            <Link to="/user-profile" className="flex bg-white items-center font-bold hover:text-blue-500">
+                Perfil 
+            </Link>
+            <Button onClick={handleLogout} className="flex items-center px-3 font-bold hover:bg-gray-500">
+                Cerrar Sesión
+            </Button>
+        </>
+    )
+}
 
 export default MobileNavLinks;

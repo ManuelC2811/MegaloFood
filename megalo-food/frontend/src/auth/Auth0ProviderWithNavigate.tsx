6d11f83674f4980dcce3a3eab6010853667c9cbd
juantorresun <117603005+juantorresun@users.noreply.github.com>
2024-05-26 16:47:51 -0500

    creacion y configuracion del archivo CheckoutButton para el checkout por parte del usuario

--- megalo-food/frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ megalo-food/frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -13,12 +13,12 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
   const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-  if (!domain  !clientId  !redirectUri  !audience) {
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("unable to initialise auth");
   }
 
   const onRedirectCallback = (appState?: AppState) => {
-    navigate(appState?.returnTo  "/auth-callback");
+    navigate(appState?.returnTo || "/auth-callback");
   };
 
   return (

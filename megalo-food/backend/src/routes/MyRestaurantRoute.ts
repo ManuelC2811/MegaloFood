ef9b9372e84700d137d31c3fa41626d9c4c1940b
Manuel Castiblanco <mcastiblancoa@unal.edu.co>
2024-05-26 11:54:51 -0500

    Se modificó el archivo MyRestaurantRoute para manejar los pedidos

--- megalo-food/backend/src/routes/MyRestaurantRoute.ts
+++ megalo-food/backend/src/routes/MyRestaurantRoute.ts
@@ -8,16 +8,44 @@ const router = express.Router();
 
 const storage = multer.memoryStorage();
 const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 5 * 1024 * 1024, //5mb
-    },
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5mb
+  },
 });
+
+router.get(
+  "/order",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.getMyRestaurantOrders
+);
+
+router.patch(
+  "/order/:orderId/status",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateOrderStatus
+);
 
 router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
 
-router.post("/", upload.single("imageFile"), validateMyRestaurantRequest , jwtCheck, jwtParse, MyRestaurantController.createMyRestaurant);
+router.post(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.createMyRestaurant
+);
 
-router.put("/", upload.single("imageFile"), validateMyRestaurantRequest , jwtCheck, jwtParse, MyRestaurantController.updateMyRestaurant);
+router.put(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant
+);
 
 export default router;

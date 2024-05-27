80b073071f236762940606775e00e0cb05a237c0
Brandon Losada <blosadas@unal.edu.co>
2024-05-26 21:24:00 -0500

    Actualización del archivo AppRoutes

--- megalo-food/frontend/src/AppRoutes.tsx
+++ megalo-food/frontend/src/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
@@ -6,20 +6,67 @@ import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 import SearchPage from "./pages/SearchPage";
+import DetailPage from "./pages/DetailPage";
+import OrderStatusPage from "./pages/OrderStatusPage";
 
 const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/"  element={<Layout showHero><HomePage /></Layout>} />
-            <Route path="auth-callback" element={<AuthCallbackPage/>} />
-            <Route path="/search/:city" element={<Layout showHero={false}><SearchPage /></Layout>} />
-            <Route element={<ProtectedRoute />}>
-                <Route path="/user-profile"  element={<Layout><UserProfilePage /></Layout>} />
-                <Route path="/manage-restaurant"  element={<Layout><ManageRestaurantPage /></Layout>} />
-            </Route>
-            <Route path="*"  element={<Navigate to="/" />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout showHero>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      <Route
+        path="/search/:city"
+        element={
+          <Layout showHero={false}>
+            <SearchPage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/detail/:restaurantId"
+        element={
+          <Layout showHero={false}>
+            <DetailPage />
+          </Layout>
+        }
+      />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/order-status"
+          element={
+            <Layout>
+              <OrderStatusPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/manage-restaurant"
+          element={
+            <Layout>
+              <ManageRestaurantPage />
+            </Layout>
+          }
+        />
+      </Route>
+
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
 
 export default AppRoutes;

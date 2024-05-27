11d9e319d02a4c7fd78f1c592e9da625f05346cc
Brandon Losada <blosadas@unal.edu.co>
2024-05-26 21:10:06 -0500

    Actualización del archivo UserProfileForm

--- megalo-food/frontend/src/forms/user-profile-form/UserProfileForm.tsx
+++ megalo-food/frontend/src/forms/user-profile-form/UserProfileForm.tsx
@@ -13,30 +13,34 @@ import { Input } from "@/components/ui/input";
 import { User } from "@/types";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect } from "react";
-import { useForm, Controller } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 const formSchema = z.object({
   email: z.string().optional(),
   name: z.string().min(1, "name is required"),
   addressLine1: z.string().min(1, "Address Line 1 is required"),
-  cellphone: z
-    .string()
-    .min(1, "Telefono movil is required")
-    .regex(/^\+?\d{1,3}\s?\d{1,4}\s?\d{4,}$/, "El formato del número de teléfono no es válido"),
   city: z.string().min(1, "city is required"),
   country: z.string().min(1, "country is required"),
 });
 
-type UserFormData = z.infer<typeof formSchema>;
+export type UserFormData = z.infer<typeof formSchema>;
 
 type Props = {
   currentUser: User;
   onSave: (userProfileData: UserFormData) => void;
   isLoading: boolean;
+  title?: string;
+  buttonText?: string;
 };
 
-const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
+const UserProfileForm = ({
+  onSave,
+  isLoading,
+  currentUser,
+  title = "User Profile",
+  buttonText = "Submit",
+}: Props) => {
   const form = useForm<UserFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: currentUser,
@@ -46,14 +50,6 @@ const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
     form.reset(currentUser);
   }, [currentUser, form]);
 
-  const formatPhoneNumber = (value: string) => {
-    return value
-      .replace(/[^\d+]/g, '')
-      .replace(/(\+\d{1,2})(\d{2,3})(\d{4,})/, (match, p1, p2, p3) => {
-        return `${p1} ${p2} ${p3}`;
-      });
-  };
-
   return (
     <Form {...form}>
       <form
@@ -61,7 +57,7 @@ const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
         className="space-y-4 bg-gray-50 rounded-lg md:p-10"
       >
         <div>
-          <h2 className="text-2xl font-bold font-raleway">Perfil de usuario</h2>
+          <h2 className="text-2xl font-bold font-raleway">{title}</h2>
           <FormDescription className="font-raleway font-semibold">
             Ver y cambiar la información de tu perfil
           </FormDescription>
@@ -112,27 +108,6 @@ const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
             )}
           />
 
-          <Controller
-            control={form.control}
-            name="cellphone"
-            render={({ field }) => (
-              <FormItem className="flex-1">
-                <FormLabel className="font-raleway font-bold">Telefono movil</FormLabel>
-                <FormControl>
-                  <Input
-                    {...field}
-                    className="bg-white"
-                    onChange={(e) => {
-                      const formattedValue = formatPhoneNumber(e.target.value);
-                      field.onChange(formattedValue);
-                    }}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
           <FormField
             control={form.control}
             name="city"
@@ -166,7 +141,7 @@ const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
           <LoadingButton />
         ) : (
           <Button type="submit" className="bg-red-500 font-raleway">
-            Enviar
+            {buttonText}
           </Button>
         )}
       </form>
